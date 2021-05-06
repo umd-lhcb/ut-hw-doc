a705bbdbdf95d9e3dce7e6fa5f4d9059e9a43d6a
Yipeng Sun <syp@umd.edu>
2021-05-06 02:22:20 +0200

    No need to specify nixpkgs input.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,8 @@
   description = "UMD UT hardward documentation website";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     flake-utils.url = "github:numtide/flake-utils";
-    mach-nix = {
-      url = "github:DavHau/mach-nix";
-      inputs = { inherit nixpkgs flake-utils; };
-    };
+    mach-nix.url = "github:DavHau/mach-nix";
   };
 
   outputs = { self, nixpkgs, flake-utils, mach-nix }:
